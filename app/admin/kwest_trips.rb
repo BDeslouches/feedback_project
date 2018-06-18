08df591fb206db6a442133e4bb56bc1b54b3df67
Raghu Betina <raghu@firstdraft.com>
2018-06-18 05:38:17 +0000

    Whitelist columns in KwestTrip for ActiveAdmin

--- app/admin/kwest_trips.rb
+++ app/admin/kwest_trips.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register KwestTrip do
+
+ permit_params :name, :start_date, :end_date, :enrollment_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
