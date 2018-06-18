b84772e97ffcdd42df0d9f688dc4db8ef499e84c
Raghu Betina <raghu@firstdraft.com>
2018-06-18 05:37:40 +0000

    Direct association between users and reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :foreign_key => "sender_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
