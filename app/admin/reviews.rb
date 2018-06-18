f1467a99faa449b6f90b5e0db027e0a7c004727d
Raghu Betina <raghu@firstdraft.com>
2018-06-18 06:20:50 +0000

    4th draft

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Review do
 
- permit_params :sender_id, :recipient_id
+ permit_params :sender_id, :recipient_id, :body
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
